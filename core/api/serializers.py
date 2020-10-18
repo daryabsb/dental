83878e9971f1d24afd02f76c6577485542c611a3
unknown <daryabsb@gmail.com>
2020-10-18 09:11:32 +0300

    Added nuxt on to of the django

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,5 +1,55 @@
+from django.contrib.auth import get_user_model, authenticate
 from rest_framework import serializers
-from core.models import Patient, Attachment
+from core.models import Patient, Attachment, Doctor
+
+class UserSerializer(serializers.ModelSerializer):
+    """Serializer for the users object"""
+   
+    class Meta:
+        model = get_user_model()
+        fields = ('email', 'password', 'name',)
+        extra_kwargs = {'password': {'write_only': True, 'min_length': 4}}
+
+    def create(self, validated_data):
+        """Create a new user with encrypted password and return it"""
+        return get_user_model().objects.create_user(**validated_data)
+    def update(self, instance, validated_data):
+        # Update a user, setting the password correctly and return it
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    """Serializer for the user authentication object"""
+    email = serializers.CharField()
+    password = serializers.CharField(
+        style={'input_type': 'password'},
+        trim_whitespace=False
+    )
+
+    def validate(self, attrs):
+        """Validate and authenticate the user"""
+        email = attrs.get('email')
+        password = attrs.get('password')
+        
+        user = authenticate(
+            request=self.context.get('request'),
+            email=email,
+            password=password
+        )
+       
+        if not user:
+            msg = _('Unable to authenticate with provided credentials')
+            raise serializers.ValidationError(msg, code='authorization')
+        
+        attrs['user'] = user
+        return attrs
 
 
 
@@ -27,6 +77,9 @@ class AttachmentSerializer(serializers.ModelSerializer):
 
 class PatientSerializer(serializers.ModelSerializer):
     # Serializer for uploading images for recipes
+    doctor = serializers.PrimaryKeyRelatedField(
+       queryset=Doctor.objects.all()
+       )
 
     class Meta:
         model = Patient
